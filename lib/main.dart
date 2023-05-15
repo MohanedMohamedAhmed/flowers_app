b2721bfa95d8139947f634b54a98a01365f98abd
MohanedMohamedAhmed <132159595+MohanedMohamedAhmed@users.noreply.github.com>
2023-05-15 18:34:01 +0300

    create log in

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flowerapp/pages/login.dart';
+import 'package:flutter/material.dart';
+void main(){
+  runApp(const MyApp());
+}
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Login(),
+    );
+    
+
+  }
+}
